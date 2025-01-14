fb4aef98e9758cf828e87b2fc049a785b442928d
lidiane41 <o email que tu usa pra acessar teu github>
2025-01-14 18:32:49 -0300

    repeticoes

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-for(let i = 0; i < 10; i++){
-console.log(`volta número: ${i}`);
+for(let i = 1; i <= 31; i++ ){
+    dia.innerHTML += `<option>${i}</option>`;
+}
+
+for(let i = 1; i <= 12; i++ ){
+    mes.innerHTML += `<option>${i}</option>`;
+}
+
+for(let i = new Date().getFullYear(); i >= new Date().getFullYear() - 50; i-- ){
+    ano.innerHTML += `<option>${i}</option>`;
+}
+
+function verificar(){
+
+    if(Number(ano.value) % 4 == 0){
+        alert(`Quem nasceu em ${dia.value}/${mes.value}/${ano.value}, nasceu em ano bissexto`)
+    }
 }
